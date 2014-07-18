b200a47a9c7d0d1ef740a12a0c6fa30f719a209c
xsimov <xsimov@gmail.com>
2014-07-18 13:29:11 +0200

    Pawns world under construction

--- lib/chess.rb
+++ lib/chess.rb
@@ -1,7 +1,16 @@
-class Chess
+require 'pry'
 
-  def movement(arr)
-    return 'ILLEGAL' if arr.empty?
-  end
-
+class ChessMove
+	def piece_team(piece_name)
+		w_pawnRules = [Proc.new { |orig, dest| orig[0]!=dest[0] }, Proc.new { |orig, dest| dest[1]-orig[1] != 1 }]
+		b_pawnRules = [Proc.new { |orig, dest| orig[0]!=dest[0] }, Proc.new { |orig, dest| orig[1]-dest[1] != 1 }]
+		return w_pawnRules if piece_name[0] == "w"
+		return b_pawnRules
+	end
+	def movement(orig, dest, piece_rules)
+		piece_rules.each do |rule|
+			return "ILLEGAL" if rule.call(orig, dest)
+		end
+		return "LEGAL"
+	end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -1,7 +1,43 @@
 require_relative '../lib/chess'
+require 'pry'
 
-describe "The movement" do
-  it "is illegal if there is no movement" do
-    expect(Chess.new.movement([])).to eql('ILLEGAL')
-  end
+describe "given a one white pawn world" do
+
+	before :each do
+		@w_p=ChessMove.new.piece_team("wP")
+	end
+
+	it "[a1 a2] is legal" do
+		expect(ChessMove.new.movement(["a", 1], ["a", 2], @w_p)).to eq("LEGAL")
+	end
+
+	it "[a1 a4] is illegal" do
+		expect(ChessMove.new.movement(["a", 1], ["a", 3], @w_p)).to eq("ILLEGAL")
+	end
+
+	it "[a1 b1] is illegal" do
+		expect(ChessMove.new.movement(["a", 1], ["b", 1], @w_p)).to eq("ILLEGAL")
+	end
+end
+
+describe "given a one black pawn world" do
+
+	before :each do
+		@b_p=ChessMove.new.piece_team("bP")
+	end
+
+	it "[a1 a2] is illegal" do
+		expect(ChessMove.new.movement(["a", 1], ["a", 2], @b_p)).to eq("ILLEGAL")
+	end
+
+	it "[a8 a7] is legal" do
+		expect(ChessMove.new.movement(["a", 8], ["a", 7], @b_p)).to eq("LEGAL")
+	end
+
+end
+
+describe "general movement rules applied to all pieces" do
+	it "must not go off the board horizontally" do
+		
+	end
 end
